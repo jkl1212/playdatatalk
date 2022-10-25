cfec503510382be58991f1387a9191af52909c78
jkl <rzhsks1226@gmail.com>
2022-10-25 21:23:34 +0900

    3 회원가입 기능

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import React, {useState} from 'react';
-import AppRouter from './Router';
+import AppRouter from 'components/Router';
+import {authService} from 'fBase';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState();
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  console.log(authService.currentUser);
+  setInterval(() => {
+    console.log(authService.currentUser);
+  }, 10000);
   return <AppRouter isLoggedIn={isLoggedIn} />;
 }
 
--- src/fBase.js
+++ src/fBase.js
@@ -2,6 +2,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
+import "firebase/auth";
 
 // import { initializeApp } from "firebase/app";
 
@@ -15,4 +16,6 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_APP_ID
   };
 
-export default firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+export const authService = firebase.auth();
