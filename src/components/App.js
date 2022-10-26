7f2e2ead665350d4b606e1489c36848b79db2d2a
jkl <rzhsks1226@gmail.com>
2022-10-26 11:54:08 +0900

    4. 로그인기능

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,27 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import AppRouter from 'components/Router';
 import {authService} from 'fBase';
 
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
-  console.log(authService.currentUser);
-  setInterval(() => {
-    console.log(authService.currentUser);
-  }, 10000);
-  return <AppRouter isLoggedIn={isLoggedIn} />;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if(user){
+        setIsLoggedIn(true);
+        
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  return (
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initialing..." }
+    </>
+  );
 }
 
 export default App;
